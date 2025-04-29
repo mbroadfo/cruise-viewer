899bb8e8eff154c2db26c768c3884fc38738a076
Mike Broadfoot <mbroadfo@yahoo.com>
2025-04-29 15:17:37 -0600

    Update vite build target for prod

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 // vite.config.js
-export default {
-    server: {
-      port: 5173
-    }
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/',
+  server: {
+    port: 5173
+  },
+  build: {
+    target: 'es2017'  // or 'es2015' if you want maximum mobile compatibility
   }
-  
+});
