b7f6e0d96f59d5b604fb97fb87d4a13bbaae709d
Mike Broadfoot <mbroadfo@yahoo.com>
2025-05-08 13:31:48 -0600

    Cleanup logging

--- src/hooks/useViewerAccessToken.js
+++ src/hooks/useViewerAccessToken.js
@@ -1,5 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { sendDebugLog } from "./utils/debugLogs";
+import { sendDebugLog } from "../utils/debugLog";
+
+let hasLoggedToken = false;
 
 export default function useViewerAccessToken() {
   const { getAccessTokenSilently } = useAuth0();
@@ -10,12 +12,15 @@ export default function useViewerAccessToken() {
         audience: "https://cruise-viewer-api",
       });
 
-      sendDebugLog({
-        type: "token-success",
-        platform: /iPhone|iPad|iPod/.test(navigator.userAgent) ? "iOS" : "other",
-        ua: navigator.userAgent,
-        tokenPresent: !!token,
-      });
+      if (!hasLoggedToken) {
+        sendDebugLog({
+          type: "token-success",
+          platform: /iPhone|iPad|iPod/.test(navigator.userAgent) ? "iOS" : "other",
+          ua: navigator.userAgent,
+          tokenPresent: !!token,
+        });
+        hasLoggedToken = true;
+      }
 
       return token;
     } catch (e) {
@@ -25,7 +30,6 @@ export default function useViewerAccessToken() {
         ua: navigator.userAgent,
         error: e.message || String(e),
       });
-
       return null;
     }
   };
